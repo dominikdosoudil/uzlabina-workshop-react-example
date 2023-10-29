ac2bf1b48acc5f8419f56c55cb4e08ed2e9da993
Dominik Dosoudil <dominik.dosoudil@yahoo.com>
2023-10-29 11:06:46 +0100

    apply few scss styles

--- src/page/ScssStyledPage.tsx
+++ src/page/ScssStyledPage.tsx
@@ -1,9 +1,27 @@
 import PageTitle from "../components/PageTitle.tsx";
+import classes from "./scss-styles.module.scss";
 
 const ScssStyledPage = () => {
   return (
-    <div>
+    <div className={classes.pageContainer}>
       <PageTitle usedStylingName="SCSS" />
+      <section className={classes.articles}>
+        <article>
+          <h3>Surviving the Long Haul</h3>
+          <p>Life for women in the trucking industry.</p>
+          <time>Jul 2016</time>
+        </article>
+        <article>
+          <h3>The Interior Stadium</h3>
+          <p>The joys of watching baseball.</p>
+          <time>Feb 1971</time>
+        </article>
+        <article>
+          <h3>Searching for Robert Johnson</h3>
+          <p>The legacy of a phantom bluesman.</p>
+          <time>Nov 2008</time>
+        </article>
+      </section>
     </div>
   );
 };
